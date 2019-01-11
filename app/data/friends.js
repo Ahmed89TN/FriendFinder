025bfaa3ddd823324de886c5e99984975cf4d459
Ahmed89TN <ahmedbenothmen045@gmail.com>
2019-01-10 21:32:36 -0800

    friendFinder

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Megan Smith",
+		photo: "https://randomuser.me/api/portraits/women/23.jpg",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Aymen Othmen",
+		photo: "https://randomuser.me/api/portraits/men/57.jpg",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Frank Ribery",
+		photo: "https://randomuser.me/api/portraits/men/81.jpg",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Chelsea Ramirez",
+		photo: "https://randomuser.me/api/portraits/women/22.jpg",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Ringo Star",
+		photo: "https://randomuser.me/api/portraits/men/13.jpg",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = friends;
